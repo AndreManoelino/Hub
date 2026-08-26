996026db5c842982c1805d8c0a90bf2c50684059
Andre Manoelino <agmphandre@gmail.com>
2026-08-26 07:53:06 -0300

    Novas migrations para o sistema realizando aqui novo sistema para banco

---
+++ Migrations/20260825202229_InitialCreate.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ControllHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Planos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    ValorMensal = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    LimiteUsuarios = table.Column<int>(type: "integer", nullable: true),
+                    LimiteAlunos = table.Column<int>(type: "integer", nullable: true),
+                    LimiteUnidades = table.Column<int>(type: "integer", nullable: true),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposEmpresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Codigo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Descricao = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposEmpresa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlanoValor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlanoId = table.Column<int>(type: "integer", nullable: false),
+                    ValorMensal = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    PercentualReajuste = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    DataInicioVigencia = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataFimVigencia = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanoValor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlanoValor_Planos_PlanoId",
+                        column: x => x.PlanoId,
+                        principalTable: "Planos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empresas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NomeFantasia = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    RazaoSocial = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    CNPJ = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    InscricaoEstadual = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Telefone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    Celular = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    CEP = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: true),
+                    Estado = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Cidade = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Bairro = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Logradouro = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Numero = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    Complemento = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    PlanoId = table.Column<int>(type: "integer", nullable: false),
+                    TipoEmpresaId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Empresas_Planos_PlanoId",
+                        column: x => x.PlanoId,
+                        principalTable: "Planos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Empresas_TiposEmpresa_TipoEmpresaId",
+                        column: x => x.TipoEmpresaId,
+                        principalTable: "TiposEmpresa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    CPF = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    SenhaHash = table.Column<string>(type: "text", nullable: false),
+                    Perfil = table.Column<int>(type: "integer", nullable: false),
+                    EmpresaId = table.Column<int>(type: "integer", nullable: true),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Empresas_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_CNPJ",
+                table: "Empresas",
+                column: "CNPJ",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_PlanoId",
+                table: "Empresas",
+                column: "PlanoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_TipoEmpresaId",
+                table: "Empresas",
+                column: "TipoEmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planos_Nome",
+                table: "Planos",
+                column: "Nome",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlanoValor_PlanoId",
+                table: "PlanoValor",
+                column: "PlanoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TiposEmpresa_Codigo",
+                table: "TiposEmpresa",
+                column: "Codigo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_CPF",
+                table: "Usuarios",
+                column: "CPF",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Email",
+                table: "Usuarios",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_EmpresaId",
+                table: "Usuarios",
+                column: "EmpresaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlanoValor");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Empresas");
+
+            migrationBuilder.DropTable(
+                name: "Planos");
+
+            migrationBuilder.DropTable(
+                name: "TiposEmpresa");
+        }
+    }
+}
---
+++ Migrations/20260825233820_AdicionarAssinaturaFaturaPagamentoPlanoValor.cs
@@ -0,0 +1,240 @@
+﻿git using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ControllHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarAssinaturaFaturaPagamentoPlanoValor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlanoValor_Planos_PlanoId",
+                table: "PlanoValor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PlanoValor",
+                table: "PlanoValor");
+
+            migrationBuilder.RenameTable(
+                name: "PlanoValor",
+                newName: "PlanosValores");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PlanoValor_PlanoId",
+                table: "PlanosValores",
+                newName: "IX_PlanosValores_PlanoId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PlanosValores",
+                table: "PlanosValores",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Assinaturas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    empresaId = table.Column<int>(type: "integer", nullable: false),
+                    PlanoId = table.Column<int>(type: "integer", nullable: false),
+                    ValorMensal = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataFim = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DiaVencimento = table.Column<int>(type: "integer", nullable: false),
+                    PercentualReajustAnual = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    AnoUltimoReajuste = table.Column<int>(type: "integer", nullable: true),
+                    Ativa = table.Column<bool>(type: "boolean", nullable: false),
+                    DataCancelamnento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    MotivoCancelamento = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assinaturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Assinaturas_Empresas_empresaId",
+                        column: x => x.empresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Assinaturas_Planos_PlanoId",
+                        column: x => x.PlanoId,
+                        principalTable: "Planos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faturas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmpresaId = table.Column<int>(type: "integer", nullable: false),
+                    AssinaturaId = table.Column<int>(type: "integer", nullable: false),
+                    PlanoId = table.Column<int>(type: "integer", nullable: false),
+                    AnoCompetencia = table.Column<int>(type: "integer", nullable: false),
+                    MesCompetencia = table.Column<int>(type: "integer", nullable: false),
+                    ValorMensal = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    ValorProporcional = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    DataInicioPeriodo = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataFimPeriodo = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Observacao = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faturas_Assinaturas_AssinaturaId",
+                        column: x => x.AssinaturaId,
+                        principalTable: "Assinaturas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Faturas_Empresas_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Faturas_Planos_PlanoId",
+                        column: x => x.PlanoId,
+                        principalTable: "Planos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmpresaId = table.Column<int>(type: "integer", nullable: false),
+                    FaturaId = table.Column<int>(type: "integer", nullable: false),
+                    ValorPago = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    FormaPagamento = table.Column<int>(type: "integer", nullable: false),
+                    IdTransacao = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    CodigoPix = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    QrCodePix = table.Column<string>(type: "text", nullable: true),
+                    DataPagamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Observacao = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pagamentos_Empresas_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pagamentos_Faturas_FaturaId",
+                        column: x => x.FaturaId,
+                        principalTable: "Faturas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assinaturas_empresaId",
+                table: "Assinaturas",
+                column: "empresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assinaturas_PlanoId",
+                table: "Assinaturas",
+                column: "PlanoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faturas_AssinaturaId",
+                table: "Faturas",
+                column: "AssinaturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faturas_EmpresaId",
+                table: "Faturas",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faturas_PlanoId",
+                table: "Faturas",
+                column: "PlanoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagamentos_EmpresaId",
+                table: "Pagamentos",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagamentos_FaturaId",
+                table: "Pagamentos",
+                column: "FaturaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlanosValores_Planos_PlanoId",
+                table: "PlanosValores",
+                column: "PlanoId",
+                principalTable: "Planos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlanosValores_Planos_PlanoId",
+                table: "PlanosValores");
+
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+
+            migrationBuilder.DropTable(
+                name: "Faturas");
+
+            migrationBuilder.DropTable(
+                name: "Assinaturas");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PlanosValores",
+                table: "PlanosValores");
+
+            migrationBuilder.RenameTable(
+                name: "PlanosValores",
+                newName: "PlanoValor");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PlanosValores_PlanoId",
+                table: "PlanoValor",
+                newName: "IX_PlanoValor_PlanoId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PlanoValor",
+                table: "PlanoValor",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlanoValor_Planos_PlanoId",
+                table: "PlanoValor",
+                column: "PlanoId",
+                principalTable: "Planos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
